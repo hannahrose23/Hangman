f926f1db5516cd80c8f9ca7515bf54cb849924b3
hannah lewandowski <hannah.lewandowski@gmail.com>
2017-08-31 19:07:04 -0500

    added to index and js files

--- Hangman.js
+++ Hangman.js
@@ -1,1 +1,17 @@
 /*hangman javascript file*/
+
+let Hangman = (function(){
+    let Hangman = {};
+
+    Hangman.init = function(config = {
+        boardSelector : "#board"
+    }){
+        Hangman.board = document.querySelector(config.boardSelector);
+        Hangman.startButton = Hangman.board.querySelector(".start-game-button");
+        Hangman.startButton.addEventListener('click',()=>{
+            console.log("click called!");
+        });
+    }
+
+    return Hangman;
+}())
